7dd8178fa245283ee9d0d6d4b6e0f9f2f59abaa5
Annija Aumale <aumale.annija@gmail.com>
2025-02-26 14:52:44 +0200

    Vehicle

---
+++ src/model/Vehicle.java
@@ -0,0 +1,79 @@
+package model;
+
+public class Vehicle {
+	//1. variables
+	private int id;
+	
+	private String title;
+	private String vehicleCode;
+	private float price;
+	private EnergyType eType;
+	
+	private static int counter = 0;
+	//2.getters
+	public int getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getVehicleCode() {
+		return vehicleCode;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public EnergyType geteType() {
+		return eType;
+	}
+	
+	//3. setters
+	
+	public void setId()
+	{
+		id = counter++;
+	}
+	
+	public void setTitle(String inputTitle) {
+		if(inputTitle != null && inputTitle.matches("[A-Za-z ]{3,20}")) {
+			title = inputTitle;
+		}
+		else
+		{
+			title = "No title";
+		}
+	}
+	
+	public void setVehicleCode() {
+		vehicleCode = id + "_" + title;
+	}
+	
+	
+	public void setPrice(float inputPrice) {
+		if(inputPrice > 0 && inputPrice < 100000) {
+			price  = inputPrice;
+		}
+		else
+		{
+			price = 100;
+		}
+	}
+	
+	public void setEnergyType(EnergyType inputEnergyType) {
+		if(inputEnergyType != null) {
+			eType = inputEnergyType;
+		}
+		else
+		{
+			eType = EnergyType.not_specified;
+		}
+	}
+	
+	//4. no arg. constr.
+	
+	//5. arg. constr.
+	
+	//6. toString
+	
+
+}
