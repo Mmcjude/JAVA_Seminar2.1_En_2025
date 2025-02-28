00a1a62c829ba9b265614468744415bb1ee62f95
Annija Aumale <aumale.annija@gmail.com>
2025-02-28 14:24:00 +0200

    Mainservice

--- src/model/Purchase.java
+++ src/model/Purchase.java
@@ -1,9 +1,13 @@
 package model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Vector;
 
-public class Purchase{
+import service.MainService;
+
+public class Purchase {
 	// variables, getters, setters, both constructors, toString + other functions(if necessary)
 	private String userCardNr;
 	private ArrayList<Vehicle> shoppingList = new ArrayList<Vehicle>();
@@ -47,7 +51,89 @@ public class Purchase{
 	}
 	
 	public String toString() {
-		return userCardNr + " bought " + shoppingList + " (" + dateTime + ")";
+		return userCardNr + " bought " + shoppingList + " (" + 
+	dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE) + " " + 
+				dateTime.getHour() + ":" + dateTime.getMinute() + ":" + dateTime.getSecond() + ")";
 	}
+	
+	
+	public void addVehicleToShoppingListByVehicleCode(String inputVehicleCode, int quantity) throws Exception{
+		if(inputVehicleCode != null && quantity > 0 && quantity <= 10) {
+			for(Vehicle tempV: MainService.getAllVehicles())
+			{
+				if(tempV.getVehicleCode().equals(inputVehicleCode))
+				{
+					if(tempV.getQuantity() >= quantity)
+					{
+	
+						for(int i = 0; i < quantity; i++) {
+							shoppingList.add(tempV);
+						}
+
+						tempV.setQuantity(tempV.getQuantity()-quantity);
+					
+					}
+					
+				}
+			}
+		}
+		else
+		{
+			throw new Exception("Code param should be not null and quantity should be 1 -10");
+		}
+	}
+	
+	public void removeVehicleFromShoppingListByVehicleCode(String inputVehicleCode) throws Exception{
+		if(inputVehicleCode != null)
+		{
+			for(Vehicle tempV : shoppingList) {
+				if(tempV.getVehicleCode().equals(inputVehicleCode)) {
+					for(Vehicle tempVV : MainService.getAllVehicles()) {
+						if(tempVV.getVehicleCode().equals(inputVehicleCode))
+						{
+							shoppingList.remove(tempV);
+							tempVV.setQuantity(tempVV.getQuantity()+1);
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			throw new Exception("Code param should be not null");
+		}
+	}
+	
+	public float calculateShoppingListValue() throws Exception {
+		if(shoppingList.isEmpty())
+		{
+			throw new Exception("Shopping list is empty");
+		}
+		
+		float sum = 0;
+		
+		for(Vehicle tempV : shoppingList) {
+			sum = sum + tempV.getPrice();
+		}
+		
+		return sum;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
